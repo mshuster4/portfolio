05c74f2f4e2e85f9417db5de1df49c4a17351183
Emroze <emrozesd@users.noreply.github.com>
2019-08-31 17:55:43 -0700

    added npm package

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+app.use(sslRedirect());
+
+app.use(sslRedirect([
+'development',
+'production'
+]));
 
 // An api endpoint that returns a short list of items
 app.get('/', (req,res) => {
-    res.send("root route")
+    res.send("Hello World")
 });
+
+app.use(sslRedirect(['production'], 301));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
